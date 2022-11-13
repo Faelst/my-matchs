7b9ba6953f9b261452af5d17a4af8e60a3bc89be
Rafael Silverio <fael_st@hotmail.com>
2022-11-13 18:57:18 -0300

    create Groups Card and Flat List

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,31 @@
+import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import * as S from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da rocket', 'Galera da rocket', 'Galera da rocket', 'Galera da rocket', 'Galera da rocket', 'Galera da rocket', 'Galera da rocket']);
+
   return (
     <S.Container>
       <Header />
+
       <Highlight title="Turmas" subtitle="Jogue com sua turma" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+            onPress={() => { }}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 24 }}
+      />
     </S.Container>
   );
 }
