2100d717d96f60b229b886e84bbfbc5fdaee5e3d
Rafael Silverio <fael_st@hotmail.com>
2022-11-14 15:01:57 -0300

    create Filter component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,4 +1,5 @@
 import { ButtonIcon } from '@components/ButtonIcon';
+import { Filter } from '@components/Filter';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
@@ -15,6 +16,9 @@ export function Players() {
         <Input placeholder="Nome do jogador" autoCorrect={false} />
         <ButtonIcon type="PRIMARY" icon="add" />
       </S.Form>
+
+      <Filter title="Time A" isActive />
+      <Filter title="Time A" isActive />
     </S.Container>
   );
-} 
+}
