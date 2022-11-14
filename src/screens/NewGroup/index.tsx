9e0f420b8505128651b517c3b25de1a8d265a30e
Rafael Silverio <fael_st@hotmail.com>
2022-11-14 02:55:42 -0300

    create input component

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,6 +3,7 @@ import * as S from './styles';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Button } from '@components/Button';
+import { Input } from '@components/Input';
 
 export function NewGroup() {
   return (
@@ -15,6 +16,8 @@ export function NewGroup() {
           title="Crie sua turma"
           subtitle="Crie sua turma e jogue com seus amigos"
         />
+
+        <Input placeholder="Nome da turma" style={{ marginBottom: 20 }} />
         <Button title="Criar turma" onPress={() => {}} />
       </S.Content>
     </S.Container>
