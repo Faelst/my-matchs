cd2a5d915ae785b51145126684620f8fc76083cb
Rafael Silverio <fael_st@hotmail.com>
2022-11-14 16:52:19 -0300

    add FlatList of players and Remove groups

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,8 +1,10 @@
+import { Button } from '@components/Button';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Filter } from '@components/Filter';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
+import { ListEmpty } from '@components/ListEmpty';
 import { PlayerCard } from '@components/PlayerCard';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
@@ -10,7 +12,7 @@ import * as S from './styles';
 
 export function Players() {
   const [team, setTeam] = useState('');
-  const [players, setPlayers] = useState([`Rafael`]);
+  const [players, setPlayers] = useState([]);
 
   return (
     <S.Container>
@@ -43,9 +45,18 @@ export function Players() {
       <FlatList
         data={players}
         keyExtractor={(item) => String(item)}
-        renderItem={({ item }) => <PlayerCard name={item} />}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
         showsVerticalScrollIndicator={false}
+        ListEmptyComponent={<ListEmpty message="Nenhum jogador encontrado" />}
+        contentContainerStyle={[
+          { paddingBottom: 20 },
+          players.length === 0 && { flex: 1 },
+        ]}
       />
+
+      <Button title="Remover turma" type="SECONDARY" />
     </S.Container>
   );
 }
