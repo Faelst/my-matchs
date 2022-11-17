d91dd71ed502fe7770533e3efbe24e02c30fe6c1
Rafael Silverio <fael_st@hotmail.com>
2022-11-17 14:58:24 -0300

    remove router header

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -7,7 +7,7 @@ const { Navigator, Screen } = createNativeStackNavigator();
 
 export function AppRoutes() {
   return (
-    <Navigator>
+    <Navigator screenOptions={{ headerShown: false }} initialRouteName="Groups">
       <Screen name="group" component={Groups} />
 
       <Screen name="new" component={NewGroup} />
